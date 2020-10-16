cc5bdb56cf55c9b9136799a62f67b50842a8a1d3
Ross Mulcare <ross.mulcare@gmail.com>
2020-10-16 19:23:10 -0400

    Initial commit

---
+++ factioneer.py
@@ -0,0 +1,39 @@
+from random import shuffle
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def gather_info():
+    return render_template('form.html')
+
+@app.route('/', methods=['POST'])  
+def display_assignments():
+    # request.form = ImmutableMultiDict
+    # ex: {"faction1":"Faction","faction2":"Faction","link1":"on","link2":"on","player1":"Player name","player2":"Player name"}
+    players = build_player_list(request.form)
+    factions = build_faction_list(request.form)
+    assignments = assign_factions(players, factions)
+    return render_template('index.html', assignments=assignments)
+
+def build_player_list(players):
+    player_list = []
+    for key, value in players.items():
+        if 'player' in key:
+            player_list.append(value)
+    return player_list
+
+def build_faction_list(factions):
+    faction_list = []
+    for key, value in factions.items():
+        if 'faction' in key:
+            faction_list.append(value)
+    return faction_list
+
+def assign_factions(players, factions):          
+    shuffle(factions)
+    if len(factions) != len(players):
+        raise Exception('The number of players and factions must match.')
+    else:
+        assignments = list(zip(players, factions))
+    return assignments
